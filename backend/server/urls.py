a512d0c6da1bdf0f851a930a02d5c4128434f018
aiteamKBC <aiteam.kbc@gmail.com>
2026-03-09 11:42:28 +0200

    V2.2.3

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
@@ -11,9 +13,13 @@ urlpatterns = [
 
     # accounts API (auth endpoints)
     path("auth/", include("accounts.urls")),
-    
-    # accounts API (evidence endpoints) 
+
+    # accounts API (evidence endpoints)
     path("api/accounts/", include("accounts.urls")),
+
+    # JWT authentication
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
 ]
 
 if settings.DEBUG:
